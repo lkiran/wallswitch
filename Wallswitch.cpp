95b56305a2ad0276196b1e0c24b83ab21ee44d44
Levent Kıran <leventkiran@hotmail.com.tr>
2020-05-29 18:37:01 +0300

    more led tests

--- Wallswitch.cpp
+++ Wallswitch.cpp
@@ -1,51 +1,60 @@
 #include "Wallswitch.h"
 
 Wallswitch::Wallswitch(int pin, int activeLow, bool pullupActive)
-    : OneButton(pin, activeLow, pullupActive)
+	: OneButton(pin, activeLow, pullupActive)
 {
-    Serial.println("Wallswitch");
+	Serial.println("Wallswitch");
 }
 
 void Wallswitch::setHaptic(HapticFeedback *haptic)
 {
-    Serial.println("Wallswitch::setHaptic");
-    this->haptic = haptic;
+	Serial.println("Wallswitch::setHaptic");
+	this->haptic = haptic;
 }
 
 void Wallswitch::setLed(RgbLed *led)
 {
-   Serial.println("Wallswitch::setLed");
-    this->led = led;
+	Serial.println("Wallswitch::setLed");
+	this->led = led;
 }
 
-void Wallswitch:: handlePress()
+void Wallswitch::handlePress()
 {
-   Serial.println(this->getPin());
-   Serial.println(" handlePress");
+	Serial.println(this->getPin());
+	Serial.println(" handlePress");
 }
 
 void Wallswitch::handleClick()
 {
-   Serial.println(this->getPin());
-   Serial.println(" handleClick");
-   this->led->setColor(RGB::blue);
+	Serial.println(this->getPin());
+	Serial.println(" handleClick");
+	if (this->led->getColor() == RGB::blue)
+		this->led->setColor(RGB::cyan);
+	else
+		this->led->setColor(RGB::blue);
 }
 
 void Wallswitch::handleDoubleClick()
 {
-   Serial.println(this->getPin());
-   Serial.println(" handleDoubleClick");
-   this->haptic->poke();
-   this->haptic->poke();
-    this->led->setColor(RGB::green);
+	Serial.println(this->getPin());
+	Serial.println(" handleDoubleClick");
+	this->haptic->poke();
+	this->haptic->poke();
+	if (this->led->getColor() == RGB::green)
+		this->led->setColor(RGB::yellow);
+	else
+		this->led->setColor(RGB::green);
 }
 
 void Wallswitch::handleLongPressStart()
 {
-   Serial.println(this->getPin());
-   Serial.println(" handleLongPressStart");
-   this->haptic->poke();
-    this->led->setColor(RGB::red);
+	Serial.println(this->getPin());
+	Serial.println(" handleLongPressStart");
+	this->haptic->poke();
+	if (this->led->getColor() == RGB::red)
+		this->led->setColor(RGB::magenta);
+	else
+		this->led->setColor(RGB::red);
 }
 
 // void Wallswitch::handleDuringLongPress()
@@ -56,6 +65,6 @@ void Wallswitch::handleLongPressStart()
 
 void Wallswitch::handleLongPressStop()
 {
-   Serial.println(this->getPin());
-   Serial.println(" handleLongPressStop");
+	Serial.println(this->getPin());
+	Serial.println(" handleLongPressStop");
 }
