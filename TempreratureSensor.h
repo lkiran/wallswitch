403996e258138d81ff5eead7f53e540806fa8bb4
LEVENT KIRAN <levent.kiran@adesso.com.tr>
2021-07-20 20:40:52 +0300

    Update TempreratureSensor.h

--- TempreratureSensor.h
+++ TempreratureSensor.h
@@ -1,25 +1,69 @@
 #include <Arduino.h>
+#include <numeric>
 
 class TempreratureSensor
 {
 private:
+    const int RESOULUTION = 12;
+    const int MAX_MV = 3300;
+    const double VOLT_TEMP_RATIO = 0.1;
+
     int _pin;
-    int _raw;
+    double _volt;
+    double _temp;
+    unsigned long updatedOn;
+    bool isAttached;
+
+    std::vector<double> measurementHistory;
 
 public:
     TempreratureSensor(int pin)
     {
         this->_pin = pin;
-        Serial.println("TempreratureSensor");
+        analogSetPinAttenuation(this->_pin, adc_attenuation_t::ADC_0db);
+        analogSetWidth(RESOULUTION);
+        analogSetClockDiv(1);
+
+        this->isAttached = adcAttachPin(this->_pin);
     }
 
-    void update()
+    void tick()
     {
-        this->_raw = analogRead(this->_pin);
+        if (this->isAttached == false)
+            return;
+
+        unsigned long now = millis();
+        if (now - updatedOn < 1000)
+            return;
+
+        updatedOn = now;
+        readMeasurement(1);
+        calculateTemperature();
+
+        Serial.print("Read ");
+        Serial.print(this->_temp);
+        Serial.println("°C");;
     }
-    
-    float tempCelcius()
+
+    double tempCelcius()
     {
-        return float(this->_raw);
+        return this->_temp;
+    }
+
+    void readMeasurement(int sampleSize)
+    {
+        this->measurementHistory.push_back(analogReadMilliVolts(this->_pin));
+        if (this->measurementHistory.size() > sampleSize)
+            this->measurementHistory.erase(this->measurementHistory.begin());
+       
+        double mVsum = std::accumulate(this->measurementHistory.begin(), this->measurementHistory.end(),
+                                       decltype(this->measurementHistory)::value_type(0));
+
+        this->_volt = mVsum / this->measurementHistory.size();
+    }
+
+    void calculateTemperature()
+    {
+        this->_temp = this->_volt * VOLT_TEMP_RATIO;
     }
 };
